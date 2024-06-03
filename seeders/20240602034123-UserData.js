e54f8e589c44101b26931948d0ad6607eb07a5e8
Luis Sanchez <luissanchezzamora23@gmail.com>
2024-06-02 21:00:20 -0500

    implement seeding

---
+++ seeders/20240602034123-UserData.js
@@ -0,0 +1,91 @@
+'use strict';
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const users = [
+      {
+        id:  uuidv4(),
+        name: 'User One',
+        lastName: 'LastName One',
+        email: 'user1@example.com',
+        password: await bcrypt.hash('password123', 10),
+        role: 'user',
+        isActive: true,
+        address: 'Address One',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:  uuidv4(),
+        name: 'User Two',
+        lastName: 'LastName Two',
+        email: 'user2@example.com',
+        password: await bcrypt.hash('password123', 10),
+        role: 'user',
+        isActive: true,
+        address: 'Address Two',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:  uuidv4(),
+        name: 'User Three',
+        lastName: 'LastName Three',
+        email: 'user3@example.com',
+        password: await bcrypt.hash('password123', 10),
+        role: 'user',
+        isActive: true,
+        address: 'Address Three',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:  uuidv4(),
+        name: 'User Four',
+        lastName: 'LastName Four',
+        email: 'user4@example.com',
+        password: await bcrypt.hash('password123', 10),
+        role: 'user',
+        isActive: true,
+        address: 'Address Four',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:  uuidv4(),
+        name: 'User Five',
+        lastName: 'LastName Five',
+        email: 'user5@example.com',
+        password: await bcrypt.hash('password123', 10),
+        role: 'user',
+        isActive: true,
+        address: 'Address Five',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    await queryInterface.bulkInsert('Users', users, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
